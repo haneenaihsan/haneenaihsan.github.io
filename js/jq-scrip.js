acc2a427d2d2113ef40e6b69191fc09e3ff98987
haneenaihsan <ihsanhaneena@gmail.com>
2025-07-13 22:23:17 +0530

    frth changed

---
+++ js/jq-scrip.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).works(function(){
+        var scrollHeight = $(window).scrollTop();
+
+        if(scrollHeight >= 50){
+            $('header').addClass('updated');
+        } else {
+            $('header').removeClass('updated');
+        }
+    });
+});
